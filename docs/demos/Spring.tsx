5aa6b056f34b4818749ec9f989b432ca18b8f6a5
tseijp <t1810394@edu.cc.uec.ac.jp>
2021-01-20 18:19:19 +0900

    :wrench:add Bounds spring demo

--- docs/demos/Spring.tsx
+++ docs/demos/Spring.tsx
@@ -1,34 +1,77 @@
 // ref:
 //     - r3f-ARC-demo - CodeSandbox
 //     - https://codesandbox.io/s/r3f-arc-demo-utyio
+// const Atom = React.forwardRef((props: any, ref) => {
+//     return <animated.group {...useAtom(props, ref)}/>
+// })
 
 import React from 'react'
 import {animated, useSprings} from 'react-spring/three'
-import {Render, useAtom} from '../../src'
+import {Render, Atom as _Atom} from '../../src'
 import * as THREE from 'three'
 
-const Atom = React.forwardRef((props: any, ref) => {
-    return <animated.group {...useAtom(props, ref)}/>
-})
+const Atom = animated(_Atom)
 
-export const Rings = ({count=100}) => {
-    const ref = React.useRef<any>();
-    const [springs] = useSprings(count, (i) => ({
+const switch0To1 = async (next: (args: any) => any) => {
+    while (1) {
+        await next({ x: 1 })
+        await next({ x: 0 })
+    }
+}
+
+export const Bounds = ({count:c=4, width:w=5}) => {
+    const ref = React.useRef<any>()
+    const [springs] = useSprings(c**2, i => ({
         ref,
-        reset: true,
         from: { x: 0 },
-        to: async (next) => {
-            while (1) {
-              await next({ x: 1 });
-              await next({ x: 0 });
-            }
-          },
+        to: switch0To1,
         config: {
             mass: 2.2,
             tension: 138,
-            friction: 129
+            friction: 20
         },
-        delay: i * count
+        delay: ( (i / c - c / 2) ** 2 + (i % c - c / 2) ** 2) * 10
+    }))
+
+    // React.useEffect(() => void (ref.current.start()));
+    return (
+        <Render>
+            <cylinderBufferGeometry attach="geometry" args={[w, w, 10, 6]}/>
+            <meshPhysicalMaterial
+                roughness={0.2}
+                metalness={1.0}
+                attach="material"
+                color="darksalmon"
+                side={THREE.DoubleSide}/>
+            <group scale={Array(3).fill(1/c) as any}
+                rotation-x={Math.PI/6}>
+                {springs.map(({ x }, i) => (
+                    <Atom
+                        key={`0${i}`}
+                        position-x={(i % c) * w * 2 - (c * w)}
+                        position-z={(i / c) * w * 2 - (c * w)}
+                        scale-y={x.to(
+                            [0, 0.1, 0.9, 1],
+                            [1, 5, 2, 1]
+                        )}/>
+                ))}
+            </group>
+        </Render>
+    )
+}
+
+export const Rings = ({count=100}) => {
+    const ref = React.useRef<any>();
+    const [springs] = useSprings(count, i => ({
+        ref,
+        from: { x: 0 },
+        to: switch0To1,
+        config: {
+            mass: 2.2,
+            tension: 138,
+            friction: 79
+        },
+        delay: 100 * i
     }));
 
     React.useEffect(() => void (ref.current.start()));
@@ -36,11 +79,14 @@ export const Rings = ({count=100}) => {
     return (
         <Render>
             <torusBufferGeometry attach="geometry" args={[10, .5, 5, 50]}/>
-            <meshPhysicalMaterial attach="material"
+            <meshPhysicalMaterial
+                roughness={0.2}
                 metalness={1}
+                attach="material"
                 color="darksalmon"
                 side={THREE.DoubleSide}/>
-            <group scale={[1/count, 1/count, 1/count]}>
+            <group scale={Array(3).fill(1 / Math.exp(count / 12)) as any}
+                rotation={[Math.PI / 2, 0, 0]}>
                 {springs.map(({ x }, i) => (
                     <Atom
                         key={`0${i}`}
@@ -48,7 +94,7 @@ export const Rings = ({count=100}) => {
                             [0, 0.1, 0.9, 1],
                             [0, -Math.PI * 0.1, Math.PI * 1.1, Math.PI]
                         )}
-                        scale={[Math.exp(10*i/count), Math.exp(10*i/count), 1]}/>
+                        scale={[Math.exp(i/12), Math.exp(i/12), 1]}/>
                 ))}
             </group>
         </Render>
