0f006e0bc34f0c3ccd830d531d1eee7cf00ac387
tseijp <t1810394@edu.cc.uec.ac.jp>
2021-01-21 14:48:57 +0900

    :wrench:add Bricks

---
+++ docs/demos/Brick.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {Atom, Poly, Render} from '../../src'
+
+const {random} = Math
+export const Bricks = () => {
+    const heights = [1, 3, 2, 2, 1, 2]
+    return (
+        <Render>
+            <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+            <meshPhongMaterial attach="material" color="darksalmon"/>
+            <Poly n={10}>
+            {(children, i) =>
+                <group>
+                    <Atom
+                        scale-x={random()*10}
+                        scale-z={random()*10}
+                        scale-y={heights[i]}/>
+                    {children &&
+                        <group position-y={heights[i-1]}>
+                            {children}
+                        </group>
+                    }
+                </group>
+            }
+        </Poly>
+        </Render>
+    )
+}
+export default Bricks
--- docs/demos/index.ts
+++ docs/demos/index.ts
@@ -1,8 +1,9 @@
 import * as Basic from './Basic'
-import * as Flow from './Flow'
 import * as Mol from './Mol'
+import * as Flow from './Flow'
+import * as Brick from './Brick'
 import * as Spring from './Spring'
 // import * as Hel from './Hel'
 // import * as Plant from './Plant'
 // import * as Swarm from './Swarm'
-export default {Basic, Flow, Mol, Spring}
+export default {Basic, Flow, Mol, Brick, Spring}
