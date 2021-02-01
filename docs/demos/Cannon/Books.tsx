766fff27193a57091c2c7f355702a7db727e0e3d
tseijp <t1810394@edu.cc.uec.ac.jp>
2021-02-01 14:29:56 +0900

    :pencil2:fix Cannon demo

--- docs/demos/Cannon/Books.tsx
+++ docs/demos/Cannon/Books.tsx
@@ -2,31 +2,78 @@ import React from 'react'
 import {Atom, Poly, Render} from '../../../src'
 import {useLoader} from 'react-three-fiber'
 import * as THREE from 'three'
+import { Physics, usePlane, useBox } from 'use-cannon'
 
+const {PI, random} = Math
 const bookURL = "http://images-jp.amazon.com/images/P/4041315220.09.MZZZZZZZ"
 
-export const Books = () => {
-    const heights = [1, 3, 2, 2, 1, 2]
-    const texture = useLoader(THREE.TextureLoader, bookURL)
+const Plane = (props: any) => {
+    const [ref] = usePlane(() => ({ mass: 0, ...props }))
     return (
-        <Render>
-            <boxBufferGeometry attach="geometry" args={[1, 1]}/>
-            <meshPhongMaterial attach="material" map={texture} />
-            <Poly n={10}>
-            {(children, i) =>
-                <group>
-                    <Atom
-                        scale-x={1}
-                        scale-z={1}
-                        scale-y={heights[i]}/>
-                    {children &&
-                        <group position-y={heights[i-1]}>
-                            {children}
-                        </group>
-                    }
-                </group>
-            }
-            </Poly>
-        </Render>
+        <mesh ref={ref} receiveShadow>
+          <planeBufferGeometry attach="geometry" args={[5, 5]} />
+          <shadowMaterial attach="material" color="#171717" opacity={0.5} />
+        </mesh>
+    )
+}
+const Book = React.forwardRef(({
+    children,
+    position=[0, 0, 0],
+    scale=[1, 1, 1],
+    ...props
+}: any) => {
+    const [ref] = useBox(() => ({
+        mass: 1,
+        args: scale || [1, 1, 1]
+    }))
+    return (
+        <>
+            <Atom ref={ref} rotation-x={Math.PI/2} scale={scale} {...props} />
+            <group position-y={position[2] + scale[2]}>
+                {children}
+            </group>
+        </>
+    )
+})
+
+export const Books = ({count:c=100}) => {
+    const widths = React.useMemo(() => [...Array(c)].map(() =>
+        random() * 1 + 3
+    ), [c])
+    const heights = React.useMemo(() => [...Array(c)].map(() =>
+        random() / c + 1 / c
+    ), [c])
+
+    const geometry = React.useRef<THREE.Geometry>()
+    React.useEffect(() => void geometry.current?.translate(0, 0, -.5), [])
+
+    const texture = useLoader(THREE.TextureLoader, bookURL)
+    // const normMap = useLoader(THREE.TextureLoader, bookURL)
+    const aspect = React.useMemo(() => texture.image.height / texture.image.width, [texture])
+    const [TextureMaterial, PaperMaterial] = React.useMemo(() => [
+            (p: any) => <meshPhongMaterial map={texture} {...p}/>,
+            (p: any) => <meshPhongMaterial color="white" {...p}/>
+    ], [texture])
+    return (
+        <Physics>
+            <Plane rotation={[-PI / 2, 0, 0]}/>
+            <Render>
+                <boxBufferGeometry attach="geometry" args={[1, 1]} ref={geometry}/>
+                <TextureMaterial attachArray="material" />
+                <PaperMaterial attachArray="material" />
+                <PaperMaterial attachArray="material" />
+                <PaperMaterial attachArray="material" />
+                <TextureMaterial attachArray="material" />
+                <TextureMaterial attachArray="material" />
+                <Poly n={heights.length - 1}>
+                {(children, i) =>
+                    <Book
+                        key={i}
+                        children={children}
+                        scale={[1*widths[i], aspect*widths[i], heights[i]]}/>
+                }
+                </Poly>
+            </Render>
+        </Physics>
     )
 }
--- docs/demos/Cannon/Bricks.tsx
+++ docs/demos/Cannon/Bricks.tsx
@@ -1,34 +1,55 @@
 import React from 'react'
 import {Atom, Poly, Render} from '../../../src'
+import * as THREE from 'three'
 // import {useLoader} from 'react-three-fiber'
-// import * as THREE from 'three'
+import niceColors from 'nice-color-palettes'
 const {random} = Math
 
-export const Bricks = () => {
-    const heights = [1, 3, 2, 2, 1, 2]
-    const bumpMap = undefined//useLoader(THREE.TextureLoader, '/JaggedStoneA_N.jpg')
+const Brick = React.forwardRef(({
+    children,
+    position=[0,0,0],
+    scale=[1,1,1],
+    ...props
+}: any, ref) => {
+    return (
+        <>
+            <Atom {...props} ref={ref} scale={scale} />
+            <group position-y={position[1] + scale[1]}>
+                {children}
+            </group>
+        </>
+    )
+})
+
+export const Bricks = ({count:c=100}) => {
+    const bumpMap = undefined  // useLoader(THREE.TextureLoader, '/JaggedStoneA_N.jpg')
+    const colors = React.useMemo(() => [...Array(c)].map(() =>
+        niceColors[17][~~(Math.random()*5)]
+    ), [c])
+
+    const heights = React.useMemo(() => [...Array(c)].map(() =>
+        random() / c + 1 / c
+    ), [c])
+
+    const geometry = React.useRef<THREE.Geometry>()
+    React.useEffect(() => void geometry.current?.translate(0, .5, 0), [])
+
     return (
         <Render>
-            <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+            <boxBufferGeometry attach="geometry" args={[1, 1, 1]} ref={geometry}/>
             <meshPhongMaterial
                 attach="material"
                 color="firebrick"
                 bumpMap={bumpMap}
                 shininess={0}/>
-            <Poly n={10}>
+            <axesHelper scale={[2, 2, 2]}/>
+            <Poly n={heights.length-1}>
             {(children, i) =>
-                <group>
-                    <Atom
-                        color={["", "", ""]}
-                        scale-x={random()*10}
-                        scale-z={random()*10}
-                        scale-y={heights[i]}/>
-                    {children &&
-                        <group position-y={heights[i-1]}>
-                            {children}
-                        </group>
-                    }
-                </group>
+                <Brick
+                    key={i}
+                    color={colors[i]}
+                    children={children}
+                    scale={[1 - .5 * random(), heights[i], 1 - .5 * random()]}/>
             }
         </Poly>
         </Render>
