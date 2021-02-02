f024081571b0dc11c239086f65913abfbefa33aa
tseijp <t1810394@edu.cc.uec.ac.jp>
2021-02-02 16:08:48 +0900

    :pencil2:fix Texts Spring demo

--- docs/demos/Spring/Texts.tsx
+++ docs/demos/Spring/Texts.tsx
@@ -1,46 +1,62 @@
-import React from 'react'
-import {animated, Spring} from 'react-spring/three'
-import {Render, Atom as _Atom} from '../../../src'
-// import {useControl as _} from 'react-three-gui'
-import {useLoader} from 'react-three-fiber'
-import FONTPATH from 'three/examples/fonts/helvetiker_regular.typeface.json'
 import * as THREE from 'three'
+import React from 'react'
+import { Canvas, createPortal, useFrame } from 'react-three-fiber'
+import { Text, Shadow, OrthographicCamera, OrbitControls } from 'drei'
+import { Render, Atom as _ } from '../../../src'
+import { animated } from 'react-spring'
 
-const Atom = animated(_Atom)
+const Atom = animated(_)
+const text = `
+const textConfig: any = {
+    color:"#dcecd3",
+    fontSize: 6,
+    maxWidth: 60,
+    lineHeight: 1,
+    letterSpacing: -0.1,
+    textAlign: "left",
+    font: "https://fonts.googleapis.com/css?family=Barlow:400,700https://fonts.gstatic.com/barlow-v4-latin-regular.woff",
+    anchorX: "center",
+    anchorY: "
+`
+const textConfig: any = {
+    color:"#dcecd3",
+    fontSize: 6,
+    maxWidth: 60,
+    lineHeight: 1,
+    letterSpacing: -0.1,
+    textAlign: "left",
+    font: "https://fonts.googleapis.com/css?family=Barlow:400,700https://fonts.gstatic.com/barlow-v4-latin-regular.woff",
+    anchorX: "center",
+    anchorY: "middle",
+}
+export function Texts () {
+    const [scene, target, camera] = React.useMemo(() => {
+        const scene = new THREE.Scene()
+        const target = new THREE.WebGLRenderTarget(2048, 2048)
+        const camera = new THREE.OrthographicCamera(-1024, 1024, -1024, 1024)
+        camera.position.set(0, 0, 10)
+        scene.background = new THREE.Color('#ff492c')
+        return [scene, target, camera]
+    }, [])
 
-export const Texts = ({count:c=10}) => {
-    const font = useLoader(THREE.FontLoader, FONTPATH as any)
-    const config = React.useMemo(() => ({
-        font, size: 40, height: 30, curveSegments: 32,
-        bevelEnabled: true, bevelThickness: 6,
-        bevelSize: 2.5, bevelOffset: 0,
-        bevelSegments: 8
-    }), [font])
-
-    console.log(font)
+    useFrame(({ gl }) => {
+        gl.setRenderTarget(target)
+        gl.render(scene, camera)
+        gl.setRenderTarget(null)
+    })
 
     return (
         <Render>
-            <textBufferGeometry attach="geometry" args={["Kinectic", config]}/>
-            <meshPhysicalMaterial attach="material" roughness={0.2} metalness={1.0} color="darksalmon"/>
-            <group>
-                {[...Array(c)].map((_, i) =>
-                    <Spring
-                        config={{mass: 10, tension: 700, friction: 30}}
-                        delay={250 * i}
-                        from={{ x: 0 }}
-                        to={async (next) => {
-                            while (true) {
-                                await next({ x: 0 })
-                                await next({ x: 1 })
-                            }
-                        }}>
-                        {({x}) =>
-                            <Atom position-x={x}/>
-                        }
-                    </Spring>
-                )}
-            </group>
+            <sphereBufferGeometry attach="geometry" args={[1, 64, 64]} />
+            <meshStandardMaterial attach="material" map={target.texture} />
+            <Atom position={[0, 0, 0]}/>
+            <Atom position={[1, 0, 0]}/>
+            <Atom position={[0, 1, 0]}/>
+            <Atom position={[0, 0, 1]}/>
+            <Atom position={[-1, 0, 0]}/>
+            <Atom position={[0, -1, 0]}/>
+            <Atom position={[0, 0, -1]}/>
+            {createPortal(<Text {...textConfig} text={text}>{text}</Text>, scene)}
         </Render>
     )
 }
